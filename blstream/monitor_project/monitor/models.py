0d23e0831fe2f18f918d7e5a912a072470116aa5
tomaszd <tomaszdudziec@gmil.com>
2016-09-27 22:01:33 +0200

    blstream
    fixup

--- blstream/monitor_project/monitor/models.py
+++ blstream/monitor_project/monitor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Site(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
