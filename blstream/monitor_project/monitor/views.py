b3281369b503c6d5ac055e8073d8155480d30482
tomaszd <tomaszdudziec@gmil.com>
2016-09-28 22:01:12 +0200

    Adding proper README cleanup

--- blstream/monitor_project/monitor/views.py
+++ blstream/monitor_project/monitor/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from monitor.models import TrackedSite
 
 
+# note this is a nice place to cache the results of index view
+# There is no need to make TrackedSite.objects.get_newest_status()
+# more often that periodic check interval.
+
 def index(request):
     """Basic view to see the most recent statuses of tracked sites"""
     newest_status = TrackedSite.objects.get_newest_status()
