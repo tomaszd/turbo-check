deb96956c0410cc3b94099c47db1489a34e0d60f
tdudziec <tdudziec@opera.com>
2016-09-28 14:44:17 +0200

    added proper views

--- blstream/monitor_project/monitor/views.py
+++ blstream/monitor_project/monitor/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 from monitor.models import TrackedSite
 
+
 def index(request):
+    """Basic view to see the most recent statuses of tracked sites"""
     newest_status = TrackedSite.objects.get_newest_status()
 
     return render(request,
                   'monitor/statuses.html',
-                  {"newest_status":newest_status}
+                  {"newest_status": newest_status}
                   )
