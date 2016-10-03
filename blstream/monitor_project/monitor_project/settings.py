1a03f7247aa409e99482bd961644911c937749d6
tdudziec <tdudziec@opera.com>
2016-10-03 10:18:37 +0200

    adding south migration for celery

--- blstream/monitor_project/monitor_project/settings.py
+++ blstream/monitor_project/monitor_project/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Celery for queueing
     'djcelery',
+    'south',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
