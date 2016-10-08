1381fb507dd40d155e0829cc4e37c8e9989f6f80
tomaszd <tomaszdudziec@gmail.com>
2016-10-08 20:56:52 +0000

    proper cron.sh

--- blstream/monitor_project/cron.sh
+++ blstream/monitor_project/cron.sh
@@ -1,4 +1,7 @@
 # this is a cron file.
-# please add it to your personal cron to monitor pages 
-# at the frequency you really want
+# please add it to your personal cron to monitor pages
+# at the frequency you really
+cd /home/tomaszd/Turbo-Check/blstream/monitor_project
+source /home/tomaszd/.virtualenvs/blstream/bin/activate
+python manage.py syncdb
 python manage.py monitor_sites
